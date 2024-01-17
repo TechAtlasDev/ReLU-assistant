12c68b9002e5d99266dc9012dc7108eac6257fc7
TechAtlasDev <srgoqui@gmail.com>
2024-01-17 14:05:59 -0500

    Mejorando el setup para la instalacion

--- setup.sh
+++ setup.sh
@@ -13,11 +13,6 @@ echo -e "\e[32m
 TechAtlasDev -> https://github.com/TechAtlasDev
 \e[0m"
 
-if [ "$EUID" -ne 0 ]; then
-    echo -e "\e[1;31m[\e[34m+\e[1;31m] \e[0mThis system requires you to run this with sudo permissions (sudo)\e[0m"
-    exit 1
-fi
-
 if ! command -v python3 &> /dev/null; then
     echo -e "\e[1;31m[\e[34m+\e[1;31m] \e[0mThis system requires Python 3 to be installed.\e[0m"
     exit 1
@@ -25,19 +20,16 @@ fi
 
 if [ -e "requirements.txt" ]; then
     echo -e "\e[1;32m[\e[34m+\e[1;32m] \e[0mInstalling requirements..."
-    sudo pip3 install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     echo -e "\e[1;31m[\e[34m+\e[1;31m] \e[0mRequirements.txt file not found in the current directory."
     exit 1
 fi
 
-if [ -d "source" ]; then
-    echo -e "\e[1;32m[\e[34m+\e[1;32m] \e[0mExporting to ReLU in the PATH variable."
-    export PATH=$PATH:./source
-    chmod +x ./source/relu
-else
-    echo "No se encontró la carpeta source en el directorio actual."
-    exit 1
-fi
+echo "--------------------------------------------------"
 
-echo "Process finished."
+echo -e "\e[1;32m[\e[34m+\e[1;32m] \e[0mEl sistema terminó de instalar las dependencias, \e[1;31mahora ejecuta el siguiente comando para que ReLU pueda ejecutarse en tu terminal:\e[0m"
+echo -e "\e[1;36m[\e[34m+\e[1;36m] \e[0m    export PATH=\$PATH:./source"
+
+echo -e "\n\e[1;32m[\e[34m+\e[1;32m] \e[0mThe system finished installing the dependencies, \e[1;31mnow run the following command so that ReLU can run in your terminal:\e[0m"
+echo -e "\e[1;36m[\e[34m+\e[1;36m] \e[0m    export PATH=\$PATH:./source"
